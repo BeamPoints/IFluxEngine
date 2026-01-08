a4390b7909f9d74655db38c78f268603921066c1
Patrick.W <beampoints@gmail.com>
2026-01-08 15:57:38 +0100

    Adding an Logger and fixed workspace settings

---
+++ TestBench/src/main.c
@@ -0,0 +1,11 @@
+#pragma once
+#include <core/logger.h>
+
+int main() 
+{
+    log_output(LOG_LEVEL_INFO, "Starting TestBench application...");
+    log_output(LOG_LEVEL_INFO, "Init Platform Layer");
+
+    return 0;
+}
+
